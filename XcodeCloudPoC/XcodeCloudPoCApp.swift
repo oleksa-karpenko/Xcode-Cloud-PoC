1ef969ee521ff15379dcfe6017776e243261b5f6
Oleksandr Karpenko <okarp@softserveinc.com>
2025-06-13 11:32:13 +0300

    Initial Commit

---
+++ XcodeCloudPoC/XcodeCloudPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudPoCApp.swift
+//  XcodeCloudPoC
+//
+//  Created by Oleksandr Karpenko on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeCloudPoCTests/XcodeCloudPoCTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudPoCTests.swift
+//  XcodeCloudPoCTests
+//
+//  Created by Oleksandr Karpenko on 13.06.2025.
+//
+
+import Testing
+@testable import XcodeCloudPoC
+
+struct XcodeCloudPoCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
