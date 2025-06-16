c82a9bfc7ded4f34305e4b752930b070dad35f44
Oleksandr Karpenko <okarp@softserveinc.com>
2025-06-16 16:32:52 +0300

    add debug script commands to see where additional tools were installed

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Installing SwiftLint
 brew install swiftlint
+
+## Checking is swiftlint visible to the shell
+which swiftlint
+
+# Installing swiftformat
 brew install swiftformat
+
+## Checking is swiftformat visible to the shell
+which swiftformat
